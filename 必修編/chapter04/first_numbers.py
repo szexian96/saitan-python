ffbd2c869b15f738fd45c3810ae1f0c67b82350a
szexian96 <58319901+szexian96@users.noreply.github.com>
2022-02-14 10:29:36 +0800

    change

--- 必修編/chapter04/first_numbers.py
+++ 必修編/chapter04/first_numbers.py
@@ -1,2 +1,5 @@
 numbers = list(range(1, 6))
 print(numbers)
+
+# for value in range(1,5):
+#   print(value)
--- 必修編/chapter04/players.py
+++ 必修編/chapter04/players.py
@@ -1,5 +1,11 @@
 players = ['charles', 'martina', 'michael', 'florence', 'eli']
+# print(players[0:3])
+# print(players[1:4])
+# print(players[:4])
+# print(players[2:])
+# print(players[-3:])
 
-print("チームの最初の3人の選手です。")
-for player in players[:3]:
-    print(player.title())
+
+# print("チームの最初の3人の選手です。")
+# for player in players[:3]:
+#     print(player.title())
